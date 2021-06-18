4e92497a97e1d0d44349071fc4256e0b97cd2269
CoocooFroggy <johnandklay@gmail.com>
2021-06-18 11:57:24 -0400

    More Windows attempted fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MainMenu.futureRestoreGUIVersion = "1.90";
+        MainMenu.futureRestoreGUIVersion = "1.90.1";
         MainMenu.main();
         /*
 
