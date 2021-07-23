b951c40d1be568362bd8dbb99d407920332c561d
CoocooFroggy <johnandklay@gmail.com>
2021-07-23 18:06:03 -0400

    - Final UI touches - 1.90 RC

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MainMenu.futureRestoreGUIVersion = "1.90.3";
+        MainMenu.futureRestoreGUIVersion = "1.90";
         MainMenu.main();
         /*
 
