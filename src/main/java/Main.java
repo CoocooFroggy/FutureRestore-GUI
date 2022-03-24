78e5f0f4ee7489972dca71d6bf2763813b7ed678
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2022-03-23 21:15:18 -0400

    Versioning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MainMenu.futureRestoreGUIVersion = "1.98.0-1";
+        MainMenu.futureRestoreGUIVersion = "1.98.00";
         MainMenu.main();
         /*
 
