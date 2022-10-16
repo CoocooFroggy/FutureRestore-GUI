83ee5f51d3e261e14e8bebecca208970b30acf70
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2022-10-16 16:05:03 -0400

    Bump version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    static final String futureRestoreGUIVersion = "1.98.2";
+    static final String futureRestoreGUIVersion = "1.98.3";
     static final boolean futureRestoreGUIPrerelease = false;
     public static void main(String[] args) {
         MainMenu.main();
